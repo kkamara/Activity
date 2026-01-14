fcebaa48d8b078192e47beffd69234ea640d2b2c
kkamara <kamaracomputers@gmail.com>
2026-01-14 12:31:20 +0000

    Show toast message on each lifecycle activity method invocation

--- app/src/main/java/com/kelvinkamara/activity/MainActivity.kt
+++ app/src/main/java/com/kelvinkamara/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kelvinkamara.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,55 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Toast.makeText(
+            this@MainActivity,
+            "onCreate called.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(
+            this@MainActivity,
+            "onStart called.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(
+            this@MainActivity,
+            "onStart called.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(
+            this@MainActivity,
+            "onPause called.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(
+            this@MainActivity,
+            "onStop called.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(
+            this@MainActivity,
+            "onDestroy called.",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
